2031b3b8d6b8c0c96b6819423ab57c2155a71f95
taylfrad <taylor.fradella@selu.edu>
2025-02-19 20:42:55 -0600

    Setup React Web and commented out swagger implementation

--- Selu383.SP25.P02.Api/Program.cs
+++ Selu383.SP25.P02.Api/Program.cs
@@ -1,10 +1,3 @@
-
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Selu383.SP25.P02.Api.Data;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using Swashbuckle.AspNetCore.SwaggerUI;
-
 namespace Selu383.SP25.P02.Api
 {
     public class Program
@@ -18,53 +11,68 @@ namespace Selu383.SP25.P02.Api
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext") ?? throw new InvalidOperationException("Connection string 'DataContext' not found.")));
 
             // This service is for Swagger UI
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Theatre API",
-                    Version = "v1",
-                    Description = "API for managing theatres"
-                });
-            });
+            /*  builder.Services.AddSwaggerGen(options =>
+              {
+                 options.SwaggerDoc("v1", new OpenApiInfo
+                 {
+                     Title = "Theatre API",
+                     Version = "v1",
+                     Description = "API for managing theatres"
+                  });
+            });   */
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
-                var app = builder.Build();
+            var app = builder.Build();
 
-                using (var scope = app.Services.CreateScope())
-                {
-                    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
-                    await db.Database.MigrateAsync();
-                    SeedTheaters.Initialize(scope.ServiceProvider);
-                }
-
-                // Configure the HTTP request pipeline.
-                if (app.Environment.IsDevelopment())
-                {
-                    app.MapOpenApi();
-
-                    app.UseSwagger();
-
-                }
-                app.UseSwaggerUI(options =>
-                {
-                    // The Swagger UI will be available at the following endpoint
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Theatre API v1");
-                    options.RoutePrefix = string.Empty; // Set Swagger UI to the root
-                });
-
-                app.UseHttpsRedirection();
-
-                app.UseAuthorization();
-
-
-                app.MapControllers();
-
-                app.Run();
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+                await db.Database.MigrateAsync();
+                SeedTheaters.Initialize(scope.ServiceProvider);
             }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+
+                //app.UseSwagger();
+
+            }
+            /*app.UseSwaggerUI(options =>
+            {
+                // The Swagger UI will be available at the following endpoint
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Theatre API v1");
+                options.RoutePrefix = string.Empty; // Set Swagger UI to the root
+            }); */
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseRouting()
+             .UseEndpoints(x =>
+                            {
+                                x.MapControllers();
+                            });
+            app.UseStaticFiles();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSpa(x =>
+                {
+                    x.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+                });
+            }
+            else
+            {
+                app.MapFallbackToFile("/index.html");
+            }
+
+            app.Run();
         }
     }
+}
 
